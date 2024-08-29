9ba482c778f64839386b2e3798618f01f2b8142c
Subash Neupane <107269356+CoolSubash@users.noreply.github.com>
2024-08-29 16:18:08 -0400

    Add files via upload

---
+++ Mean.R
@@ -0,0 +1,17 @@
+#Author: Subash, Date:08/22/24, Purpose:Test the measures of descriptive stats
+
+#How to calculate standard deviation in R
+X <- c(1,2,3,4,5)
+Y <- 100
+sd(X)
+
+
+#How to calculate mean in R
+mean(X)
+
+# How to calculate median in R
+median(X)
+
+# How to calculate mode in R
+#Will need to create our own function
+
